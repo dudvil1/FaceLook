e393215f97346a1f2a80325bdfe542c766e54c48
dudu <dudvil1@gmail.com>
2020-01-30 09:53:26 +0200

    second server

---
+++ server/routes/default.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var api = express.Router();
+var DefaultController = require('../controllers/default');
+
+api.get('/', DefaultController.help);
+api.get('/status', DefaultController.status);
+
+module.exports = api;
