527ba48da5ef226c07113790a4bd5c579a2900ef
dudu <dudvil1@gmail.com>
2020-02-20 08:14:31 +0200

    save post

---
+++ server/repository/DbConnection.js
@@ -0,0 +1,4 @@
+const connectionString =
+  "server=(localdb)\\sqlexpress;Database=FaceLook;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+
+module.exports = connectionString ;
--- server/repository/dbmaneger.js
+++ server/repository/dbmaneger.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const bcrypt = require("../services/bcryptService");
-const moment = require('moment');
+const moment = require("moment");
+const connectionString = require("./DbConnection");
 const sql = require("msnodesqlv8");
-const connectionString =
-  "server=(localdb)\\sqlexpress;Database=FaceLook;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 
 async function find(table, key, userKey, callback) {
   console.log("dbManeger: find call()");
-  console.log("userkey", userKey);
+
   const query = `SELECT * FROM ${table} WHERE ${key} = '${userKey}'`;
 
   await sql.query(connectionString, query, (err, rows) => {
@@ -34,10 +33,10 @@ async function verifyAccount(userId, callback) {
   const query = `UPDATE Users
                 SET active = '1'
                 WHERE _id = '${userId}'`;
-                await sql.query(connectionString, query, (err, rows) => {
-                  if (err) console.log("verifyAccountErr:",err);
-                  callback("ok");
-                });
+  await sql.query(connectionString, query, (err, rows) => {
+    if (err) console.log("verifyAccountErr:", err);
+    callback("ok");
+  });
 }
 
 async function changePassword(userEmail, newpassword) {
@@ -56,37 +55,46 @@ async function changePassword(userEmail, newpassword) {
     });
 }
 
-async function addPost(post,callback){
+async function addPost(post, callback) {
   console.log("dbManeger: addPost call()");
-  
+
   post.post_id = new mongoose.Types.ObjectId();
+  post.image = "";
   post.date = moment().unix().toString();
-  const query = `INSERT INTO Posts VALUES( '${post.post_id}','${image}' , '${post.text}' , '${post.date} ,
-   '${post.locationLocationLat}' , ${post.locationLocationLng}) , ${post.text}`;
+  
+  const query = `INSERT INTO Posts VALUES( '${post.post_id}','${post.user[0]._id}','${post.image}' , '${post.text}' , '${post.date}' ,
+   '${post.locationLocationLat}' , '${post.locationLocationLng}' , '${post.title}')`;
 
-   await sql.query(connectionString, query, (err, res) => {
-   callback(post);
+  await sql.query(connectionString, query, (err, res) => {
+    if(err)console.log("from addPost",err);
+    callback(post);
   });
-} 
+}
 
-async function addTag(tag,callback) {
+async function addTag(tag, callback) {
   console.log("dbManeger: addTag call()");
 
-  tag.tag_id = new mongoose.Types.ObjectId();
-  const query =  `INSERT INTO tags VALUES( '${tag.tag_id}', '${tag.tag}'`;
-  await sql.query(connectionString, query, (err, res) => {
-    callback(tag);
-   });
+  find("Tags", "Text", tag.tag, result => {
+    if (result.length < 1) {
+      tag.tag_id = new mongoose.Types.ObjectId();
+      const query = `INSERT INTO Tags
+                     VALUES( '${tag.tag_id}', '${tag.tags}')`;
+      sql.query(connectionString, query, (err, res) => {
+        if(err)console.log("from addtag",err);
+        callback(tag);
+      });
+    } else callback({});
+  });
 }
 
 async function addPost_Tag(post_tag, callback) {
   console.log("dbManeger: Post_Tag call()");
 
-  const query =  `INSERT INTO Post_Tag VALUES( '${post_tag.post_id}', '${post_tag.tag_id}'`;
+  const query = `INSERT INTO Post_Tag VALUES( '${post_tag.post_id}', '${post_tag.tag_id}')`;
   await sql.query(connectionString, query, (err, res) => {
+    if(err)console.log("from addPost_tag",err);
     callback(post_tag);
-   });
-
+  });
 }
 
 module.exports = {
