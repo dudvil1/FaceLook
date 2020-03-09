c0abbe0001ec5c5cc18a6a5b38a2d4d1c2e90c8d
guykem123 <guykem1@gmail.com>
2020-03-09 15:58:50 +0200

    remove

--- server/services/bcryptService.js
+++ server/services/bcryptService.js
@@ -1,28 +1,3 @@
-// const bcrypt = require("bcryptjs");
-
-// async function checkPassword(reqPassword, userPassword) {
-//   console.log("bycrptService: checkPassword call()");
-
-//   await bcrypt.compareSync(reqPassword, userPassword, (err, result) => {
-//     if (result) return true;
-//     return false;
-//   });
-// }
-
-// async function createHashPassword(password) {
-//   console.log("bycrptService: createHashPassword call()");
-
-//   return await bcrypt.hashSync(password, 10, (err, hash) => {
-//     if (err) return false;
-//     return hash;
-//   });
-// }
-
-// module.exports = {
-//   checkPassword,
-//   createHashPassword
-// };
-
 module.exports = (bcrypt) => {
 
   function checkPassword(reqPassword, userPassword) {
