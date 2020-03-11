d8ce269a18af75b15ea474c105dd3a3623ad2c8c
dudu <dudvil1@gmail.com>
2020-03-11 14:09:37 +0200

    update

--- server/repository/DbConnection.js
+++ server/repository/DbConnection.js
@@ -1,7 +1,1 @@
-//   const connectionString =
-//   "server=(localdb)\\sqlexpress;Database=FaceLook;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-
-///guy connection
-const connectionString =process.env.SQL_CONNECTION_STRING
-
-module.exports = connectionString;
+module.exports =  connectionString =process.env.SQL_CONNECTION_STRING;
