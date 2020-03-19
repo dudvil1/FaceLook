88fd66cfcd5abeadb73fe134bea8247ca54f2aa8
guykem123 <guykem1@gmail.com>
2020-03-19 20:48:36 +0200

    update

--- server/repository/DbConnection.js
+++ server/repository/DbConnection.js
@@ -1,1 +1,3 @@
-module.exports =  connectionString = "server=(localdb)\\sqlexpress;Database=FaceLook;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const connectionString =process.env.SQL_CONNECTION_STRING
+
+module.exports = connectionString;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const container = require('./containerConfig')
 const http = container.get('http');
 const app = container.get('app');
 const logger = container.get('loggerService');
-const dbManager = container.get('dbManager');
 const socketService = container.get('socketService');
 const port = process.env.port || 3000;
 const server = http.createServer(app);
