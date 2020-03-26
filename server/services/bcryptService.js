e56ff973bc89f747f03356f069b2cb97bb798565
guykem123 <50095264+guykem123@users.noreply.github.com>
2020-03-26 21:43:27 +0200

    update

--- server/services/bcryptService.js
+++ server/services/bcryptService.js
@@ -1,6 +1,6 @@
 module.exports = (bcrypt) => {
-  const hashKey = Number(process.env.BCRYPT_HASH_KEY); 
-
+  const hashKey = Number(process.env.BCRYPT_HASH_KEY) || 10;
+  
   function checkPassword(reqPassword, userPassword) {
     console.log("bycrptService: checkPassword call()");
 
@@ -10,7 +10,7 @@ module.exports = (bcrypt) => {
   function createHashPassword(password) {
     console.log("bycrptService: createHashPassword call()");
 
-    return bcrypt.hashSync(password,hashKey);
+    return bcrypt.hashSync(password, hashKey);
   }
 
   return {
