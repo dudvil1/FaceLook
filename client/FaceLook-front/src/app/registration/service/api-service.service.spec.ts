ac9d1858b6d82261c2baa288082afa0c2c960377
guykem123 <50095264+guykem123@users.noreply.github.com>
2020-04-06 11:02:58 +0300

    register service tested

--- client/FaceLook-front/src/app/registration/service/api-service.service.spec.ts
+++ client/FaceLook-front/src/app/registration/service/api-service.service.spec.ts
@@ -3,8 +3,10 @@ import { registrationApiService } from './api-service.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { ApiConfigService } from '../../common/service/api-config.service';
+import { Observable } from 'rxjs';
+import { ISuccessResponse } from 'src/app/common/model/successResponse';
 
-describe("postsFilterService", () => {
+fdescribe("postsFilterService", () => {
   let service: registrationApiService;
   let httpClient: HttpClient
   let httpTestingController: HttpTestingController
@@ -38,4 +40,62 @@ describe("postsFilterService", () => {
     expect(service).toBeTruthy();
   });
 
+  it("login() ApiRequest- test url,method and response are correct", () => {
+    const { registrationUrl, login } = apiConfigService.registrationApi
+    const url = registrationUrl + login
+    const mockSuccessResponse: ISuccessResponse = { message: "success", token: "1214c3fvd3rcasc" }
+    const reqOnservable = service.login({ email: "re@gwmail.com", password: '123456' })
+
+    TestApi_REST_Request(reqOnservable, 'Post', url, mockSuccessResponse)
+  });
+
+  it("getResetCodePassword() ApiRequest- test url,method and response are correct", () => {
+    const { registrationUrl, getResetCodePassword } = apiConfigService.registrationApi
+    const url = registrationUrl + getResetCodePassword
+    const mockSuccessResponse: ISuccessResponse = { message: "success" }
+    const reqOnservable = service.getResetCodePassword("re@gwmail.com")
+
+    TestApi_REST_Request(reqOnservable, 'patch', url, mockSuccessResponse)
+  });
+
+  it("getResetCodePassword() ApiRequest- test url,method and response are correct", () => {
+    const { registrationUrl, verifyAccount } = apiConfigService.registrationApi
+    const url = registrationUrl + verifyAccount
+    const mockSuccessResponse: ISuccessResponse = { message: "success verify" }
+    const reqOnservable = service.verifyAccount("1242342954nj5345nj")
+
+    TestApi_REST_Request(reqOnservable, 'patch', url, mockSuccessResponse)
+  });
+
+  it("register() ApiRequest- test url,method and response are correct", () => {
+    const { registrationUrl, register } = apiConfigService.registrationApi
+    const url = registrationUrl + register
+    const mockSuccessResponse: ISuccessResponse = { message: "success register" }
+    const reqOnservable = service.register({name:"ddd",email:"sss@sss.ss",password:'wwwww'})
+
+    TestApi_REST_Request(reqOnservable, 'post', url, mockSuccessResponse)
+  });
+
+  it("register() ApiRequest- test url,method and response are correct", () => {
+    const { registrationUrl, forgetPassword } = apiConfigService.registrationApi
+    const url = registrationUrl + forgetPassword
+    const mockSuccessResponse: ISuccessResponse = { message: "success register" }
+    const reqOnservable = service.updatePassword({ email:'gggg@gg.gg',newPassword:'gggg',confirmPassword:'gggg'})
+
+    TestApi_REST_Request(reqOnservable, 'patch', url, mockSuccessResponse)
+  });
+
+  function TestApi_REST_Request(requestObservable: Observable<any>, method: string, url: string, expectedResult: any) {
+    requestObservable.subscribe(
+      res => {
+        expect(res).toEqual(expectedResult)
+      }
+    );
+
+    const request = httpTestingController.expectOne(url);
+    expect(request.request.method).toEqual(method.toUpperCase(), `request should be a ${method.toUpperCase()} Method`);
+    request.flush(expectedResult);
+  }
+
 });
+
