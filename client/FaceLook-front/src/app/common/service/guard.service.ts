dab1f21b59f9e999799d728fc475f6156c2e82f5
guykem123 <50095264+guykem123@users.noreply.github.com>
2020-04-07 11:50:46 +0300

    end unit test in angular

--- client/FaceLook-front/src/app/common/service/guard.service.ts
+++ client/FaceLook-front/src/app/common/service/guard.service.ts
@@ -2,29 +2,32 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { NavigatorService } from './navigator.service';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GuardService implements CanActivate{
+export class GuardService implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
 
     const isJwtNeeded = route.data["jwtNeeded"]
-    const token = localStorage.getItem("token")
-    const isTokenExist = token == null ? false:true;
+    const token = this.storage.getToken()
+    const isTokenExist = token == null ? false : true;
 
-    
+
     //reRoute if cant access
-    if(isJwtNeeded && !isTokenExist){
+    if (isJwtNeeded && !isTokenExist) {
       this.navigateService.goToLogin();
     }
-    if(!isJwtNeeded && isTokenExist){
+    if (!isJwtNeeded && isTokenExist) {
       this.navigateService.goToHomePage();
     }
 
-    return isJwtNeeded ? isTokenExist:!isTokenExist
+    return isJwtNeeded ? isTokenExist : !isTokenExist
   }
 
-  constructor(private navigateService: NavigatorService) { }
+  constructor(
+    private navigateService: NavigatorService,
+    private storage: StorageService) { }
 }
