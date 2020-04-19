6459c6a0d4bc64a7e4b41f41d50b80cb64e85720
dudu <dudvil1@gmail.com>
2020-04-19 13:41:31 +0300

    before

--- server/test/mocks/jwtServiceMock.js
+++ server/test/mocks/jwtServiceMock.js
@@ -4,15 +4,12 @@ module.exports = {
     isTokenExpire
 };
 
-//should return string
 function createToken(user) {
     return JSON.stringify(user)
 };
-//should return object
 function decodeToken(token) {
     return {}
 };
-//should return boolean
 function isTokenExpire(token) {
     return token ? true : false;
 };
--- server/test/routesTest/registartion.spec.js
+++ server/test/routesTest/registartion.spec.js
@@ -1,23 +1,44 @@
 const { chai, assert, should, expect, sinon } = require("../baseTest");
+const rgstCtrl = '';
 
-describe("test the Registration routes", function () {
+describe("test for the Registration routes", function() {
   this.timeout(20000);
   const server = "http://localhost:3000/registration";
-  it("test the /register routes", done => {
-    const route = chai.request(server)
+  const registration = require('../../routes/registration');
+
+  it("test the /login routes - send valid parameters", done => {
+    chai
+      .request(server)
       .post("/login")
       .send({ email: "dudvil1@gmail.com", password: "1234" })
-
-
-    CheckRouteExist(route, done);
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.status).to.not.equal(200);
+        expect(res.body).to.not.haveOwnProperty("erorr");
+        done();
+      });
   });
+  it("test the /login routes - send unValid parameters", done => {
+    chai
+      .request(server)
+      .post("/login")
+      .send({ email: "test@gmail.com", password: "1234" })
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.status).to.not.equal(404);
+        expect(res.body).to.not.haveOwnProperty("erorr");
+        done();
+      });
+  });
+
+  it('test the /register route', (done)=> {
+    chai
+      .request(server)
+      .post('/register')
+      .send({name:'testName', email:'test@gmail.com', password:'testpassword'})
+      .end( (err,res) => {
+        done();
+      })
+  })
+
 });
-function CheckRouteExist(route, done) {
-  route.end(function (err, res) {
-    expect(err).to.be.null;
-    expect(res.status).to.not.equal(404);
-    expect(res.body).to.not.haveOwnProperty('erorr');
-    done();
-  });
-}
-
